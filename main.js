e8cd0aa305b2fd3960a3f22fae8ec05cc24665c1
Paul Kline <pauliankline@gmail.com>
2019-07-13 02:12:16 -0500

    status bar, and a little better css for image shrinking. a little too small on mobile though

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ console.log("version", "1.2.1");
 let songStart = null;
 let predictedEnd = null;
 let gap = 0;
+let DELAY = 5; //seconds.
 function startTime() {
   Array.prototype.forEach.call(clocks, function(el) {
     el.innerHTML = new Date().toLocaleTimeString();
@@ -19,7 +20,7 @@ function startTime() {
       predictedEnd.setMilliseconds(predictedEnd.getMilliseconds() + app.songdat.song_length);
       gap = predictedEnd.getTime() - songStart.getTime();
     }
-    const elapsedTime = new Date().getTime() - songStart.getTime();
+    const elapsedTime = new Date().getTime() - (songStart.getTime() + DELAY * 1000);
     const percent = Math.max(0, Math.min((elapsedTime / gap) * 100, 100));
     app.progressObject.width = "" + percent + "%";
   }
@@ -67,7 +68,28 @@ var app = new Vue({
         hour: "numeric",
         minute: "2-digit"
       });
+    },
+    showStatus: function() {
+      return this.songdat.song_length && predictedEnd && predictedEnd.getTime() + DELAY <= new Date().getTime();
+    },
+    artistTooltip: function() {
+      return this.albumurl ? "View Artist on Spotify" : "";
+    },
+    albumTooltip: function() {
+      return this.albumurl ? "View Album on Spotify" : "";
     }
+  },
+  mounted() {
+    console.log("mounted");
+    if (INCLUDE_PLAY_BUTTON) {
+      handleIncludePlay();
+    }
+    setTimeout(() => {
+      $(function() {
+        console.log("data toggle");
+        $('[data-toggle="tooltip"]').tooltip();
+      });
+    }, 1500);
   },
   created() {
     console.log("about to axios:");
@@ -80,65 +102,109 @@ var app = new Vue({
         recur_updateSong(obj);
       }, 10000);
     }
-    function updateSong(obj) {
-      axios.get("https://kjhk.org/nowplaying.php").then(json => {
-        // console.log("received data:", json.data);
-        //modified api to handle last X songs, so check if array and just get first:
-        if (json.data instanceof Array) {
-          json.data = json.data[0];
-        }
-        if (obj.songdat.timestamp !== json.data.timestamp) {
-          obj.songdat = json.data;
-          setTitle(obj.songdat.song + " · " + obj.songdat.artist);
-          console.log("updating song");
-          //reset progress bar;
-          songStart = null;
-          predictedEnd = null;
-          app.progressObject.width = "0%";
+    async function updateSong(obj) {
+      const songdata = await getNowPlaying();
+      if (obj.songdat.timestamp == songdata.timestamp) {
+        return; // nothing to do!
+      }
+      //NEW SONG INFO!!!
+      obj.songdat = songdata;
+      setTitle(obj.songdat.song + " - " + obj.songdat.artist);
+      console.log("updating song");
+      //reset progress bar;
+      resetProgressBar();
+      console.log("calling with:", obj.songdat.artist, obj.songdat.album);
 
-          console.log("calling with:", obj.songdat.artist, obj.songdat.album);
-          document.getElementsByTagName;
-          axios
-            .get("http://kjhkstream.org:3210/albumart", {
-              params: {
-                artist: obj.songdat.artist,
-                album: obj.songdat.album
-              }
-            })
-            .then(data => {
-              console.log(data.data);
-              let albuminfo;
-              let imageurl;
+      let albumArtData = await getAlbumArtData(obj.songdat.artist, obj.songdat.album);
+      // let albumArtData = await getAlbumArtData("Silversun Pickups", "Widows Weeds");
+      let albuminfo;
+      let imageurl;
 
-              try {
-                app.linkTarget = "_blank";
-                console.log(albuminfo);
-                albuminfo = data.data.albums.items[0];
-                imageurl = albuminfo.images[0].url; //zero is biggest.
-                app.spotifyAlbumUrl = albuminfo.external_urls.spotify;
-                app.spotifyArtistUrl = albuminfo.artists[0].external_urls.spotify;
-              } catch (e) {
-                console.log("no album art received:", e);
-                imageurl = "";
-                app.spotifyAlbumUrl = "#";
-                app.spotifyArtistUrl = "#";
-                app.linkTarget = "";
-              }
+      const attempt = function() {
+        app.linkTarget = "_blank";
+        albuminfo = albumArtData.albums.items[0];
+        imageurl = albuminfo.images[0].url; //zero is biggest.
+        app.spotifyAlbumUrl = albuminfo.external_urls.spotify;
+        app.spotifyArtistUrl = albuminfo.artists[0].external_urls.spotify;
+      };
+      try {
+        attempt();
+      } catch (e) {
+        console.log("no album art received:", e);
+        imageurl = "";
+        app.spotifyAlbumUrl = "#";
+        app.spotifyArtistUrl = "#";
+        app.linkTarget = "";
 
-              app.albumurl = imageurl;
-              console.log(app);
-            })
-            .catch(e => {
-              console.log("uhoh. I caught something: ", e);
+        //see if you can fix it.
+        let albumtext = obj.songdat.album.trim().toLowerCase();
+
+        //check if the problem was because it was a SINGLE
+        if (albumtext.endsWith("single") || albumtext == "na" || albumtext == "n/a" || albumtext == "[na]") {
+          console.log("trying to fix album art away from single");
+          albumArtData = await getAlbumArtData(obj.songdat.artist, obj.songdat.song);
+          try {
+            attempt();
+          } catch (e) {
+            console.log("nope. can't fix. I tried because I thought it was a single or NA.", e);
+          }
+        } else {
+          //see if it contains apostrophies. IDK why spotify sometiems has a problem with that?
+
+          //see if it's the clean/edited version.
+          const matches = albumtext.match(/[\[\(]?clean[\]\)]?|[\[\(]?edited[\[\(]?|[\[\(]?ep[\]\(]?|'/g);
+          if (matches) {
+            matches.forEach(m => {
+              albumtext = albumtext.replace(m, "");
             });
+            albumtext = albumtext.trim();
+            console.log(
+              "trying again, but this time looking for album: '",
+              albumtext,
+              "' instead of: ",
+              obj.songdat.album
+            );
+            albumArtData = await getAlbumArtData(obj.songdat.artist, albumtext);
+            try {
+              attempt();
+            } catch (e) {
+              console.log("nope. can't fix. I tried because I thought it was an edited/clean version.", e);
+            }
+          }
         }
-      });
+      }
+      app.albumurl = imageurl;
     }
   }
 });
-
+function resetProgressBar() {
+  songStart = null;
+  predictedEnd = null;
+  app.progressObject.width = "0%";
+}
+async function getNowPlaying() {
+  let response = await axios.get("https://kjhk.org/nowplaying.php");
+  let answer = response.data;
+  if (response.data instanceof Array) {
+    answer = response.data[0];
+  }
+  // console.log(answer);
+  return answer;
+}
+async function getAlbumArtData(artist, album) {
+  const result = await axios.get("http://kjhkstream.org:3210/albumart", {
+    params: {
+      artist: artist,
+      album: album
+    }
+  });
+  console.log("result is", result);
+  return result.data;
+}
 function setTitle(str) {
-  document.getElementsByTagName("title")[0].innerHTML = replaceTag(str).replace("Â", "");
+  document.getElementsByTagName("title")[0].innerHTML = replaceTag(str)
+    .replace("Â", "")
+    .replace("Â", "");
   console.log("str was", str, "title set to:", replaceTag(str).replace("Â", ""));
 }
 let tagsToReplace = {
@@ -153,4 +219,38 @@ function replaceTag(tag) {
 
 function safe_tags_replace(str) {
   return str.replace(/[&<>]/g, replaceTag);
+}
+
+function handleIncludePlay() {
+  var music = document.getElementById("music"); // id for audio element
+  var duration; // Duration of audio clip
+  var pButton = document.getElementById("pButton"); // play button
+
+  //Play and Pause
+  function play() {
+    // start music
+    if (music.paused) {
+      music.play();
+      setTitle(app.songdat.song + " - " + app.songdat.artist);
+      // remove play, add pause
+      document.getElementById("play").style.display = "none";
+      document.getElementById("pause").style.display = "block";
+    } else {
+      // STOP music
+      music.pause();
+      setTitle("KJHK Stream");
+      // remove pause, add play
+      document.getElementById("play").style.display = "block";
+      document.getElementById("pause").style.display = "none";
+    }
+  }
+
+  // Gets audio file duration
+  music.addEventListener(
+    "canplaythrough",
+    function() {
+      duration = music.duration;
+    },
+    false
+  );
 }
---
+++ musicplayer.js
@@ -0,0 +1,37 @@
+var music = document.getElementById("music"); // id for audio element
+var duration; // Duration of audio clip
+var pButton = document.getElementById("pButton"); // play button
+
+//Play and Pause
+function play() {
+  // start music
+  if (music.paused) {
+    music.play();
+    setTitle(app.songdat.song + " · " + app.songdat.artist);
+    // remove play, add pause
+    document.getElementById("play").style.display = "none";
+    document.getElementById("pause").style.display = "block";
+  } else {
+    // STOP music
+    music.pause();
+    setTitle("KJHK Stream");
+    // remove pause, add play
+    document.getElementById("play").style.display = "block";
+    document.getElementById("pause").style.display = "none";
+  }
+}
+
+//John's code:
+//Volume controls:
+
+//var myPlayer = document.getElementById("audioplayer");
+
+function getVolume() {
+  alert(music.volume);
+}
+function increaseVolume() {
+  music.volume = 0.5;
+}
+function decreaseFullVolume() {
+  music.volume = 0.1;
+}
